4ae1695b07dd4869d090bf31dc71cb3bcc28aea4
ZHIHAN-HE <hezhihan2008@gmail.com>
2015-09-14 13:45:48 +0800

    Create Lab1-Step-36.php

---
+++ Lab1-Step-36.php
@@ -0,0 +1,13 @@
+<?php
+$a1=$_POST ["a"];
+$b1=$_POST ["b"];
+$c1=$_POST ["c"];
+$d1=$_POST ["d"];
+$e1=$_POST ["e"];
+
+echo "<p>Greetings $a1 $b1</p>";
+echo "<p>Your User name is: $c1 and your password is: $d1</p>";
+echo "<p>Your cellphone number is $e1</p>";
+
+echo  "<p><hr color-'darkblue' size='4'>End of Part3 </p>";
+?>
